7e24b642e3cb32a2da9e06f3de33397e829def58
egor69ok <egor69ok@yandex.ru>
2026-02-06 00:14:26 +0300

    Create soft.sh

---
+++ soft.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Установка приложений
+sudo pacman -S --noconfirm firefox telegram-desktop godot blender gimp \
+steam ardour surge-xt-vst3 thunar thunar-archive-plugin thunar-volman \
+gvfs tumbler ffmpegthumbnailer imv mpv
+
+# Софт из AUR
+yay -S --noconfirm visual-studio-code-bin outline-manager outline-client
