d6fef611b465f13fff7809b005de29a23556289d
egor69ok <resed69ok@gmail.com>
2026-02-08 21:46:25 +0300

    fix4

--- install_start/01_aur_i.sh
+++ install_start/01_aur_i.sh
@@ -2,6 +2,7 @@
 # Графика и шрифты
 sudo pacman -S --noconfirm hyprland waybar swww wofi \
 ttf-font-awesome noto-fonts swaylock-effects swaync grim slurp swappy
+sudo pacman -S ttf-jetbrains-mono-nerd sudo pacman
 
 # Установка yay (AUR)
 git clone https://aur.archlinux.org
